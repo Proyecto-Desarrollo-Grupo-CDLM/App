0054a3b13dca45bce4167879f39f45e858c42aae
Ignacio Masalles <nachomasalles.nm@gmail.com>
2025-11-27 11:41:37 -0300

    generamos proxy y los component

---
+++ MundiFavs/angular/src/app/proxy/index.ts
@@ -0,0 +1,5 @@
+import * as Calificaciones from './calificaciones';
+import * as CitySearch from './city-search';
+import * as Destinos from './destinos';
+import * as System from './system';
+export { Calificaciones, CitySearch, Destinos, System };
