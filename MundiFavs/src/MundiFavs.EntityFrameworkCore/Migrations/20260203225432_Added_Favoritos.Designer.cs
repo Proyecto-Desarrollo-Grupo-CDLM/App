9cf6c23105766542848dcf9f1de1bfb346418b34
JeroDelavalle <jerodelavalle@gmail.com>
2026-02-03 20:45:26 -0300

    Creacion de Lista de Favoritos y sus respectivas funcionalidades

--- MundiFavs/src/MundiFavs.EntityFrameworkCore/Migrations/20260203225432_Added_Favoritos.Designer.cs
+++ MundiFavs/src/MundiFavs.EntityFrameworkCore/Migrations/20260203225432_Added_Favoritos.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MundiFavs.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace MundiFavs.Migrations
 {
     [DbContext(typeof(MundiFavsDbContext))]
-    partial class MundiFavsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260203225432_Added_Favoritos")]
+    partial class Added_Favoritos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
